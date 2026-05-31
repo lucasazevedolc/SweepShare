0d53413e8dee4c7afedaa0b2057a4f850311ed49
Lucas Azevedo <lucasazevedolc@gmail.com>
2026-05-31 16:41:54 +0200

    doc: implement declarative OpenAPI Swagger definition with global JWT security

---
+++ src/main/java/project/sweepshare/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package project.sweepshare.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SweepShare API",
+                version = "1.0.0",
+                description = "Backend API documentation for SweepShare - Managing task rotation and cleaning schedules in shared apartments (WGs)."
+        ), security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT")
+public class SwaggerConfig {}
